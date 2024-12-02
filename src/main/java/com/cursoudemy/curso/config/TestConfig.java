e8e81c5176db1a413439fc3f2d366c89393083ba
Lucas Ferreira <lucasfalvesferreira@gmail.com>
2024-12-02 17:08:26 -0300

    Product-OrderItem one-to-many association And Payment, one-to-one association

--- src/main/java/com/cursoudemy/curso/config/TestConfig.java
+++ src/main/java/com/cursoudemy/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cursoudemy.curso.entities.Category;
 import com.cursoudemy.curso.entities.Order;
 import com.cursoudemy.curso.entities.OrderItem;
+import com.cursoudemy.curso.entities.Payment;
 import com.cursoudemy.curso.entities.Product;
 import com.cursoudemy.curso.entities.User;
 import com.cursoudemy.curso.entities.enums.OrderStatus;
@@ -82,5 +83,7 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
 	}
 }
--- src/main/java/com/cursoudemy/curso/entities/Order.java
+++ src/main/java/com/cursoudemy/curso/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.cursoudemy.curso.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,29 +17,33 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
+public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd '|' HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	private Integer orderStatus;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-	
+
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+
 	public Order() {
 	}
 
@@ -71,10 +76,10 @@ public class Order implements Serializable{
 	}
 
 	public void setOrderStatus(OrderStatus orderStatus) {
-		if(orderStatus != null) {
-			this.orderStatus = orderStatus.getCode();	
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
 		}
-		 
+
 	}
 
 	public User getClient() {
@@ -84,9 +89,16 @@ public class Order implements Serializable{
 	public void setClient(User client) {
 		this.client = client;
 	}
-	
-	
-	public Set<OrderItem> getItems(){
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Set<OrderItem> getItems() {
 		return items;
 	}
 
